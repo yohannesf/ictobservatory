abededb355fa2a84b404572abb7876541404edfa
yohannesf <yohannes.fikre@gmail.com>
2023-02-16 15:28:25 +0300

    update secret_key

--- ictobservatory/settings.py
+++ ictobservatory/settings.py
@@ -43,7 +43,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
