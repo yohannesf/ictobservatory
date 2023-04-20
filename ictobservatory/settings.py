42dacae95bbb12aa40a7f1fb4e2a3b16e3607afa
yohannesf <yohannes.fikre@gmail.com>
2023-04-20 14:57:47 +0300

    setting changed auth

--- ictobservatory/settings.py
+++ ictobservatory/settings.py
@@ -59,7 +59,8 @@ INSTALLED_APPS = [
     'jazzmin',
 
     "django.contrib.admin",
-    "django.contrib.auth",
+    # "django.contrib.auth",
+    'core.authconfig.CustomAuthConfig',
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
