a6353f36bb039507ba08da723fda534baa39b503
yohannesf <yohannes.fikre@gmail.com>
2023-06-20 13:49:26 +0300

    settings - password validator

--- ictobservatory/settings.py
+++ ictobservatory/settings.py
@@ -170,12 +170,25 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 12,
+        },
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+    {
+        # Add the custom validators
+        "NAME": "core.backends.UppercaseCharacterValidator",
+    },
+    {
+        "NAME": "core.backends.SpecialCharacterValidator",
+    },
+    {
+        "NAME": "core.backends.NumberValidator",
     },
 ]
 
